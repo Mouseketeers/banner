2565a7b4dce4e61074c98fa6a471f197e66894cb
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:57:05 +1300

    Converted to PSR-2

--- code/PageBanner.php
+++ code/PageBanner.php
@@ -1,63 +1,72 @@
 <?php 
-class PageBanner extends DataObject {
+class PageBanner extends DataObject
+{
 
-	static $db =  array(
-		'BannerTitle' => 'Varchar(255)',
-		'BannerContent' => 'HTMLText',
-		'BannerLinkTitle' => 'Varchar(255)',
-		'BannerWidth' => 'Varchar',
-		'BannerHeight' => 'Varchar',
-		'DefaultBanner' => 'Boolean'
-	);
-	static $has_one = array(
-		'BannerImage' => 'Image',
-		'BannerLink' => 'SiteTree',
-		'Page' => 'Page'
-	);
-	function getCMSFields_forPopup() {
-		$fields = new FieldSet();
-		$fields->push( new FileUploadField('BannerImage', "Banner Image" ));
-		if(PageBannerDecorator::$content_enabled) {
-			$fields->push( new TextField('BannerTitle','Title'));
-			if (PageBannerDecorator::$enable_html_editor) {
-				$fields->push( new SimpleTinyMCEField('BannerContent','Content'));
-			}
-			else {
-				$fields->push( new TextAreaField('BannerContent','Content'));
-			}
-		}
-		$page_dropdown = new SimpleTreeDropdownField('BannerLinkID', 'Link to page');
-		$page_dropdown->setEmptyString('-- None --');
-		$fields->push( $page_dropdown );
-		if(PageBannerDecorator::$use_default_banner) {
-			$fields->push( new CheckboxField('DefaultBanner', 'Set this as the default banner' ));
-		}
-		$fields->push( new LiteralField('DOM-fix','<div style="height:35px">&nbsp;</div>'));
-		return $fields;
-	}
-	function getIfDefaultBanner() {
-		if($this->DefaultBanner) return 'Default';
-	}
-	function getImageTitle() {
-		return $this->BannerImage()->Title;
-	}
-	function getContentSummary() {
-		return $this->dbObject('BannerContent')->Summary();
-	}
-	function getThumbmailOfBannerImage() {
-		return $this->BannerImage()->StripThumbnail();
-	}
-	function getBannerLinkURL() {
-		return $this->BannerLink()->Title;
-	}
-	public function onBeforeWrite() {
-		parent::onBeforeWrite();
-		if($this->DefaultBanner) {
-			$current_default_banner = DataObject::get_one('PageBanner','DefaultBanner = 1');
-			if($current_default_banner) {
-				$current_default_banner->DefaultBanner = 0;
-				$current_default_banner->write();
-			}
-		}
-	}
+    public static $db =  array(
+        'BannerTitle' => 'Varchar(255)',
+        'BannerContent' => 'HTMLText',
+        'BannerLinkTitle' => 'Varchar(255)',
+        'BannerWidth' => 'Varchar',
+        'BannerHeight' => 'Varchar',
+        'DefaultBanner' => 'Boolean'
+    );
+    public static $has_one = array(
+        'BannerImage' => 'Image',
+        'BannerLink' => 'SiteTree',
+        'Page' => 'Page'
+    );
+    public function getCMSFields_forPopup()
+    {
+        $fields = new FieldSet();
+        $fields->push(new FileUploadField('BannerImage', "Banner Image"));
+        if (PageBannerDecorator::$content_enabled) {
+            $fields->push(new TextField('BannerTitle', 'Title'));
+            if (PageBannerDecorator::$enable_html_editor) {
+                $fields->push(new SimpleTinyMCEField('BannerContent', 'Content'));
+            } else {
+                $fields->push(new TextAreaField('BannerContent', 'Content'));
+            }
+        }
+        $page_dropdown = new SimpleTreeDropdownField('BannerLinkID', 'Link to page');
+        $page_dropdown->setEmptyString('-- None --');
+        $fields->push($page_dropdown);
+        if (PageBannerDecorator::$use_default_banner) {
+            $fields->push(new CheckboxField('DefaultBanner', 'Set this as the default banner'));
+        }
+        $fields->push(new LiteralField('DOM-fix', '<div style="height:35px">&nbsp;</div>'));
+        return $fields;
+    }
+    public function getIfDefaultBanner()
+    {
+        if ($this->DefaultBanner) {
+            return 'Default';
+        }
+    }
+    public function getImageTitle()
+    {
+        return $this->BannerImage()->Title;
+    }
+    public function getContentSummary()
+    {
+        return $this->dbObject('BannerContent')->Summary();
+    }
+    public function getThumbmailOfBannerImage()
+    {
+        return $this->BannerImage()->StripThumbnail();
+    }
+    public function getBannerLinkURL()
+    {
+        return $this->BannerLink()->Title;
+    }
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        if ($this->DefaultBanner) {
+            $current_default_banner = DataObject::get_one('PageBanner', 'DefaultBanner = 1');
+            if ($current_default_banner) {
+                $current_default_banner->DefaultBanner = 0;
+                $current_default_banner->write();
+            }
+        }
+    }
 }
--- code/PageBannerSubsites.php
+++ code/PageBannerSubsites.php
@@ -1,21 +1,27 @@
 <?php
-class PageBannerSubsites extends DataObjectDecorator {
-	function extraStatics() {
-		return array(
-			'has_one' => array(
-				'Subsite' => 'Subsite',
-			)
-		);
-	}
-	function augmentSQL(SQLQuery &$query) {
-		if(Subsite::$disable_subsite_filter) return;
-		$subsiteID = (int)Subsite::currentSubsiteID();
-		$where = '"PageBanner"."SubsiteID" IN ('.$subsiteID.')';
-		$query->where[] = $where;
-	}
-	function onBeforeWrite() {
-		if (!$this->owner->ID && !$this->owner->SubsiteID) {
-			$this->owner->SubsiteID = Subsite::currentSubsiteID();
-		}
-	}
+class PageBannerSubsites extends DataObjectDecorator
+{
+    public function extraStatics()
+    {
+        return array(
+            'has_one' => array(
+                'Subsite' => 'Subsite',
+            )
+        );
+    }
+    public function augmentSQL(SQLQuery &$query)
+    {
+        if (Subsite::$disable_subsite_filter) {
+            return;
+        }
+        $subsiteID = (int)Subsite::currentSubsiteID();
+        $where = '"PageBanner"."SubsiteID" IN ('.$subsiteID.')';
+        $query->where[] = $where;
+    }
+    public function onBeforeWrite()
+    {
+        if (!$this->owner->ID && !$this->owner->SubsiteID) {
+            $this->owner->SubsiteID = Subsite::currentSubsiteID();
+        }
+    }
 }
